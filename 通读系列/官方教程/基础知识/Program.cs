3d029a36c245bb11645cec7e5213e65a225ffd1a
Radium <2043869032@qq.com>
2023-04-01 21:45:45 +0800

    chore: commit

---
+++ 通读系列/官方教程/基础知识/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+var dictionary = new Dictionary<string, string>();
+dictionary.Add("mac", "2");
+
+
+Console.Out.WriteLine(dictionary["mac"]);

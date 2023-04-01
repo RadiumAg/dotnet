745b6790bcd4f97774911afbbbe3d8477bbada05
Radium <2043869032@qq.com>
2023-04-01 22:21:23 +0800

    chore: commit

--- 通读系列/官方教程/基础知识/Program.cs
+++ 通读系列/官方教程/基础知识/Program.cs
@@ -1,6 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var dictionary = new Dictionary<string, string>();
 dictionary.Add("mac", "2");
+Console.Out.WriteLine(new a()["v"]);
+
+class a
+{
+    public string _v = "1";
+
+    public string v { get { return _v; } }
+
+    public string? this[string key]
+    {
+        get
+        {
+            var type = this.GetType();
+            var propertyInfo = type.GetProperty(key);
+            var value = propertyInfo?.GetValue(this) as string;
+            return value;
+        }
+    }
+}
 
 
-Console.Out.WriteLine(dictionary["mac"]);

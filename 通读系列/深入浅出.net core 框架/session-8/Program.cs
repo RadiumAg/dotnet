0397c8989fc5c13f29ed352c717656d0ccfda678
Radium <2043869032@qq.com>
2023-04-05 22:37:42 +0800

    chore: commit

---
+++ 通读系列/深入浅出.net core 框架/session-8/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.Caching.Memory;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var cache = new ServiceCollection()
+               .AddMemoryCache()
+               .BuildServiceProvider()
+               .GetRequiredService<IMemoryCache>();
+cache.Set("m", "dotnet");
+cache.TryGetValue("m1", out var value);
+Console.WriteLine(value);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

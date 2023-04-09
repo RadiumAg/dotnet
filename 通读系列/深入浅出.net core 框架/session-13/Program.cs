c625d246cfbc7ea22e639496c35ac935f4ebd0f4
Radium <2043869032@qq.com>
2023-04-09 22:55:41 +0800

    chore: commit

--- 通读系列/深入浅出.net core 框架/session-13/Program.cs
+++ 通读系列/深入浅出.net core 框架/session-13/Program.cs
@@ -1,1 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
+Task<int>  downloading =  
+
+
+static async Task<int> DownloadDocsMainPageAsync(){
+    Console.WriteLine($"{nameof(DownloadDocsMainPageAsync)}:About to start downloading.");
+   
+   var client = new HttpClient();
+
+   byte[] content = await client.GetByteArrayAsync("https://www.bing.com/");
+}

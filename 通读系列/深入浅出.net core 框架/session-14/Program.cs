131cb9cf9618674d2500e0855e4d5e64d4c4f39b
Radium <2043869032@qq.com>
2023-04-09 23:46:22 +0800

    chore: commit

--- 通读系列/深入浅出.net core 框架/session-14/Program.cs
+++ 通读系列/深入浅出.net core 框架/session-14/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+
+// var value = 0;
+
+// Parallel.For(0, 10000, _ =>
+// {
+//     Interlocked.Add(ref value, 1);
+// });
+
+// Console.WriteLine(value);
+
+var location = 1;
+var value = 3;
+var compared = 1;
+Interlocked.CompareExchange(ref location, value, compared);
+Console.WriteLine(location);

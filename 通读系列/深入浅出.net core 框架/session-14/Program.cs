4bc7d1669714f22b0f3f713d82b60f4b704c941d
2043869032@qq.com <2043869032@qq.com>
2023-04-10 09:38:06 +0800

    chore: commit

--- 通读系列/深入浅出.net core 框架/session-14/Program.cs
+++ 通读系列/深入浅出.net core 框架/session-14/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-
 // var value = 0;
 
 // Parallel.For(0, 10000, _ =>
@@ -11,8 +10,36 @@
 
 // Console.WriteLine(value);
 
-var location = 1;
-var value = 3;
-var compared = 1;
-Interlocked.CompareExchange(ref location, value, compared);
-Console.WriteLine(location);
+// var location = 1;
+// var value = 3;
+// var compared = 1;
+// Interlocked.CompareExchange(ref location, value, compared);
+// Console.WriteLine(location);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var thread = new Thread(DoWork);
+        thread.Start();
+        Thread.Sleep(50);
+
+        Console.ReadKey();
+    }
+
+    private static volatile bool _isCompleted = false;
+
+    static void DoWork()
+    {
+        var spinWait = new SpinWait();
+
+        while (!_isCompleted)
+        {
+            spinWait.SpinOnce();
+            Console.WriteLine($"自旋次数：{spinWait.Count}, 下一次调用触发上下文切换和内核宣旋转：{spinWait.NextSpinWillYield}");
+        }
+
+        Console.WriteLine("Waiting is complete");
+    }
+
+}

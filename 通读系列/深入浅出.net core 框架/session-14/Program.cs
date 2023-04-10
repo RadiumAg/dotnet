52010df3c1d676608b36e771f33bfe2656ae9835
2043869032@qq.com <2043869032@qq.com>
2023-04-10 11:11:27 +0800

    chore: commit

--- 通读系列/深入浅出.net core 框架/session-14/Program.cs
+++ 通读系列/深入浅出.net core 框架/session-14/Program.cs
@@ -44,3 +44,106 @@
 
 // }
 
+// var spinLock = new SpinLock();
+// var list = new List<int>();
+
+
+// Parallel.For(0, 10000000, r =>
+// {
+//     bool lockTaken = false;  // 释放锁
+//     try
+//     {
+//         spinLock.Enter(ref lockTaken); // 进入锁
+//         list.Add(r);
+//     }
+//     finally
+//     {
+//         if (lockTaken) spinLock.Exit(false); //释放
+//     }
+
+//     Console.WriteLine(list.Count);
+//     // 输出 10000000
+// });
+
+
+// class Program
+// {
+//     private static readonly object _object = new object();
+
+//     public static void Print()
+//     {
+//         bool _lock = false;
+//         Monitor.Enter(_object, ref _lock);
+
+//         try
+//         {
+//             for (int i = 0; i < 5; i++)
+//             {
+//                 Thread.Sleep(100);
+//                 Console.Write(i + ",");
+//             }
+//             Console.WriteLine();
+//         }
+//         finally
+//         {
+//             if (_lock)
+//             {
+//                 Monitor.Exit(_object);
+//             }
+//         }
+//     }
+
+
+//     static void Main()
+//     {
+//         var threads = new Thread[3];
+
+//         for (int i = 0; i < 3; i++)
+//         {
+//             threads[i] = new Thread(Print);
+//         }
+
+
+//         foreach (var t in threads)
+//         {
+//             t.Start();
+//         }
+
+//         Console.ReadLine();
+//     }
+// }
+
+
+class Program
+{
+    private static Mutex mutex = new Mutex(false);
+
+    static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            var thread = new Thread(Print);
+            thread.Name = "Thread: "  + i;
+            thread.Start();
+        }
+        Console.ReadKey();
+    }
+
+
+    static void Print()
+    {
+        Console.WriteLine($"{Thread.CurrentThread.Name} 即将开始进行处理");
+
+        try
+        {
+            mutex.WaitOne();
+            Console.WriteLine($"开始：{Thread.CurrentThread.Name} 处理中。。。");
+            Thread.Sleep(2000);
+            Console.WriteLine($"完成：{Thread.CurrentThread.Name}");
+        }
+        finally
+        {
+            mutex.ReleaseMutex();
+        }
+    }
+}

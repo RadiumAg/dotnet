3b7b8f74aee766545921dcdb0c48847f56c0548d
2043869032@qq.com <2043869032@qq.com>
2023-04-13 11:51:31 +0800

    chore: commit

--- 通读系列/Effective C#/Program.cs
+++ 通读系列/Effective C#/Program.cs
@@ -1,16 +1,4 @@
-﻿Third();
-
-
-static void Third()
-{
-    long? a = 1000000000000000000;
-    int? b = a as int?;
-
-    Console.WriteLine(b);
-}
-
-
-// 内插字符
+﻿// 内插字符
 Console.WriteLine($"The value of pi is {Math.PI:F2}");
 Console.WriteLine($"The value of pi is {(true ? Math.PI.ToString() : Math.PI.ToString("F2"))}");
 
@@ -28,6 +16,66 @@ var output = $@"The First five items are:{"path".Take(5).Select(n => $@"Item:{n.
 FormattableString second = $"It's the {DateTime.Now.Day} of the {DateTime.Now.Month} month";
 
 
+// 9 尽量避免装箱与取消装箱这两种操作
+int firstNumber = 100;
+int secondNumber = 200;
+int thirdNumber = 300;
+Console.WriteLine($"A few numbers: {firstNumber},{secondNumber},{thirdNumber}");
+
+var attendees = new List<Person>();
+Person p = new Person { Name = "OldName" };
+
+attendees.Add(p);
+var p2 = attendees[0];
+p2.Name = "New Name";
+
+Console.WriteLine(attendees[0].ToString());
+
+// 10. 只有在对应新版基类与现有子类质检的冲突时才应该使用new修饰符
+var d = new MyOtherClass();
+d.MagicMethod();
+
+var  cl2 = d as MyClass;
+cl2.MagicMethod();
+
+static string ToGerman(FormattableString src)
+{
+    return string.Format(null, System.Globalization.CultureInfo.CreateSpecificCulture("de-de"), src.Format, src.GetArguments());
+}
+
+static string ToFrenchCanada(FormattableString src)
+{
+    return string.Format(null, System.Globalization.CultureInfo.CreateSpecificCulture("fr-CA"), src.Format, src.GetArguments());
+}
+
+
+public class MyClass
+{
+    public void MagicMethod()
+    {
+        Console.WriteLine("MyClass");
+    }
+}
+
+
+public class MyOtherClass : MyClass
+{
+    public new void MagicMethod()
+    {
+        Console.WriteLine("MyOtherClass");
+    }
+}
+
+public struct Person
+{
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
 class Test
 {
     public static void ExceptionMessage(object thisCantBeNull)
@@ -43,7 +91,7 @@ class Test
 public class EventSource
 {
     private int counter = 0;
-    private EventHandler<int> Updated;
+    private EventHandler<int>? Updated;
 
     public void RaiseUpdates()
     {
@@ -52,20 +100,8 @@ public class EventSource
         //     handler(this, counter);
 
         counter++;
-        Updated?.Invoke(this,counter);
+        Updated?.Invoke(this, counter);
     }
 }
 
 
-// 9 尽量避免装箱与取消装箱这两种操作
-var firstNumber = 100;
-Console.WriteLine($"A few numbers: {}");
-static string ToGerman(FormattableString src)
-{
-    return string.Format(null, System.Globalization.CultureInfo.CreateSpecificCulture("de-de"), src.Format, src.GetArguments());
-}
-
-static string ToFrenchCanada(FormattableString src)
-{
-    return string.Format(null, System.Globalization.CultureInfo.CreateSpecificCulture("fr-CA"), src.Format, src.GetArguments());
-}

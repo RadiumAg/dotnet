e5db565d78e5dc0a70bad8780e947d0c5730c8ec
2043869032@qq.com <2043869032@qq.com>
2023-04-18 11:20:16 +0800

    chore: commit

--- 通读系列/c#编程魔法书/Program.cs
+++ 通读系列/c#编程魔法书/Program.cs
@@ -1,4 +1,7 @@
-﻿StringFormatDemo.Main();
+﻿using System.Globalization;
+
+StringFormatDemo.Main();
+StringCompareDemo.Main();
 
 class StringFormatDemo
 {
@@ -26,5 +29,28 @@ class StringFormatDemo
         var path1 = @"c:\china-pub\C#\sample-code";
         var path2 = "c:\\china-pub\\C#\\sample-code";
         Console.WriteLine("两个路径是相同的:\n{0}\n{1}", path1, path2);
+    }
+}
+
+class StringCompareDemo
+{
+    public static void Main()
+    {
+        object a = 1, b = 1;
+        Console.WriteLine(a == (object)1);
+        Console.WriteLine(a == b);
+
+        string c = "1", d = "1";
+        Console.WriteLine(c == d);
+        Console.WriteLine(c == "1");
+
+
+        Console.WriteLine(string.Compare("1", "2"));
+        Console.WriteLine(string.Compare("a", "A"));
+        Console.WriteLine(string.Compare("a", "A", true));
+
+        Console.WriteLine(string.Compare("财经传讯公司", "房地产及按揭"));
+        Console.WriteLine(string.Compare("财经传讯公司", "房地产及按揭", false, new CultureInfo("zh-CN")));
+        Console.WriteLine(string.Compare("财经传讯公司", "房地产及按揭", false, new CultureInfo("en-US")));
     }
 }

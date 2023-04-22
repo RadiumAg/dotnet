5234be59cbffff57a8c0fbe56925586924205608
Radium <2043869032@qq.com>
2023-04-22 23:46:21 +0800

    chore: commit

--- 通读系列/c#编程魔法书/Program.cs
+++ 通读系列/c#编程魔法书/Program.cs
@@ -398,4 +398,28 @@ public class ReflectionDemo
             Console.WriteLine("------------");
         }
     }
+
+
+    static void loadForm(string[] args)
+    {
+        var assembly = Assembly.LoadFrom(args[0]);
+        var type = assembly.GetType("DemoClass");
+        var sAdd = type.GetMethod("Add", BindingFlags.Static | BindingFlags.NonPublic);
+        var result = (int)sAdd.Invoke(null, new object[] { 1, 2 });
+
+        Console.WriteLine($"Static Add: {result}");
+        var ctor = type.GetConstructor(new Type[] { typeof(int) })
+        var dcInst = ctor.Invoke(new object[] { 10 });
+        var property = type.GetProperty("Value");
+        result = (int)property.GetValue(dcInst);
+        Console.WriteLine($"Value:{result}");
+        MethodInfo add = type.GetMethod("Add", new Type[] { typeof(int) });
+        add.Invoke(dcInst, new object[] { 3 });
+        property = type.GetProperty("Value");
+        result = (int)property.GetValue(dcInst);
+        Console.WriteLine($"Instance Add:{result}");
+    }
 }
+
+
+
